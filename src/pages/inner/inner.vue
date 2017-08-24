3d20786fe891301375d547ed71e7cd0d387ab00b
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-08-24 11:00:26 +0800

    正文

--- src/pages/inner/inner.vue
+++ src/pages/inner/inner.vue
@@ -1,17 +1,35 @@
 <template>
     <section class="inner">
-        
-    正文页
-
+        <section ></section>
     </section>
 </template>
 
 <script>
+import { getNewsList, getNewsData } from "api/index.js"
 export default {
     name:'inner',
     data() {
         return {
-            
+            newsData:{}
+        }
+    },
+    created() {
+        this._getNewsData(this._parseUrl())
+    },
+    methods:{
+        _parseUrl() {
+            return this.$route.query.id
+        },
+        _getNewsData(id) {
+            return new Promise((resolve, reject) => {
+                getNewsData(id).then((res) => {
+                    let data = res.data;
+                    this.newsData = data
+                    console.log('------------------------------------');
+                    console.log(data.body);
+                    console.log('------------------------------------');
+                })
+            })
         }
     }
 }
