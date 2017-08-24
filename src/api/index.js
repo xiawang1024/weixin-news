b6bcb52729fbcdc966817873abae7db2ad4aa739
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-08-24 10:13:29 +0800

    默认单页60条

--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,9 @@ axios.defaults.baseURL = 'http://admin.hndt.com'
 /**
  * 
  * @param {number} page 页数
- * @param {number} rows 单页数量 默认40条
+ * @param {number} rows 单页数量 默认60条
  */
-const getNewsList = (page, rows = 40) => axios.get('/api/page?channel_id=969&template_id=153&rows=' + rows + '&page=' + page)
+const getNewsList = (page, rows = 60) => axios.get('/api/page?channel_id=969&template_id=153&rows=' + rows + '&page=' + page)
 
 
 export {
