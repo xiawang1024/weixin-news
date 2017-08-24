d950a92aa23c9c74b9264c2a617eb09439a62cba
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-08-24 11:00:00 +0800

    newsData接口

--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,13 @@ axios.defaults.baseURL = 'http://admin.hndt.com'
  * @param {number} rows 单页数量 默认60条
  */
 const getNewsList = (page, rows = 60) => axios.get('/api/page?channel_id=969&template_id=153&rows=' + rows + '&page=' + page)
-
+/**
+ * 
+ * @param {number} id 索引id
+ */
+const getNewsData = (id) => axios.get('/api/page?template_id=164&article_id=' + id)
 
 export {
-    getNewsList
+    getNewsList,
+    getNewsData
 }
